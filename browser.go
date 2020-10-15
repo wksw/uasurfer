08b839f93455666968849c256c594cafdf576178
kawa <kai.wang@royole.com>
2020-10-15 09:04:14 +0800

    rollback

--- browser.go
+++ browser.go
@@ -37,14 +37,14 @@ func (u *UserAgent) evalBrowserName(ua string) bool {
 		case strings.Contains(ua, "wechat/"):
 			u.Browser.Name = BrowserWechat
 
-		case strings.Contains(ua, "postman"):
-			u.Browser.Name = BrowserPostman
+		// case strings.Contains(ua, "postman"):
+		// 	u.Browser.Name = BrowserPostman
 
-		case strings.Contains(ua, "flexpai"):
-			u.Browser.Name = BrowserFlexPai
+		// case strings.Contains(ua, "flexpai"):
+		// 	u.Browser.Name = BrowserFlexPai
 
-		case strings.Contains(ua, "flexpai2"):
-			u.Browser.Name = BrowserFlexPai2
+		// case strings.Contains(ua, "flexpai2"):
+		// 	u.Browser.Name = BrowserFlexPai2
 
 		case strings.Contains(ua, "opr/") || strings.Contains(ua, "opios/"):
 			u.Browser.Name = BrowserOpera
@@ -115,14 +115,14 @@ notwebkit:
 	case strings.Contains(ua, "wechat/"):
 		u.Browser.Name = BrowserWechat
 
-	case strings.Contains(ua, "postman"):
-		u.Browser.Name = BrowserPostman
+	// case strings.Contains(ua, "postman"):
+	// u.Browser.Name = BrowserPostman
 
-	case strings.Contains(ua, "flexpai"):
-		u.Browser.Name = BrowserFlexPai
+	// case strings.Contains(ua, "flexpai"):
+	// u.Browser.Name = BrowserFlexPai
 
-	case strings.Contains(ua, "flexpai2"):
-		u.Browser.Name = BrowserFlexPai2
+	// case strings.Contains(ua, "flexpai2"):
+	// u.Browser.Name = BrowserFlexPai2
 
 	case strings.Contains(ua, "msie") || strings.Contains(ua, "trident"):
 		u.Browser.Name = BrowserIE
